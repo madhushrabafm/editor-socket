a364d147ba47d43cddf40e6dbc25e483b8382b8e
madhushrabafm <madhushraba.das@fortmindz.in>
2024-07-25 15:22:04 +0530

    codemirror added with errors

--- src/components/EditorPlayground.jsx
+++ src/components/EditorPlayground.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+// import "codemirror/mode/javascript/javascript";
+// import "codemirror/theme/dracula.css";
+// import "codemirror/addon/edit/closetag";
+// import "codemirror/addon/edit/closebrackets";
+// import "codemirror/lib/codemirror";
+// import CodeMirror from "codemirror";
 
 const EditorPlayground = () => {
-  return <div>EditorPlayground</div>;
+  const editorRef = useRef();
+  // useEffect(() => {
+  //   const init = async () => {
+  //     const editor = CodeMirror.fromTextArea(
+  //       document.getElementById("EditorPlayground"),
+  //       {
+  //         mode: { name: "javascript", json: true },
+  //         theme: "dracula",
+  //         autoCloseTags: true,
+  //         autoCloseBrackets: true,
+  //         lineNumbers: true,
+  //       }
+  //     );
+  //   };
+  // }, []);
+
+  return (
+    <div>
+      <textarea name="" id="EditorPlayground"></textarea>
+    </div>
+  );
 };
 
 export default EditorPlayground;
